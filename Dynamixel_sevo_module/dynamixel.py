d0660eb1519b0467ccf08d85def6d6e7642fe0b2
Raptor701 <contactadress456@gmail.com>
2018-07-09 09:49:41 -0400

    Add error exception, if servo is not connected

--- Dynamixel_sevo_module/dynamixel.py
+++ Dynamixel_sevo_module/dynamixel.py
@@ -33,6 +33,7 @@ def init(ID):
     print("Succeeded to change the baudrate")
  dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
  if dxl_comm_result != COMM_SUCCESS:
+    raise Exception("Something went wrong, probably one of the servos is't connected or out of power supply")
     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
  elif dxl_error != 0:
     print("%s" % packetHandler.getRxPacketError(dxl_error))
