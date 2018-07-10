20818bef3e0bd9dda7e1743e72a5f2f679d5b733
Vukong <40642450+Vukong@users.noreply.github.com>
2018-07-11 00:02:56 +0300

    Def version of dictionary

--- recognition/dictionary_1.py
+++ recognition/dictionary_1.py
@@ -4,8 +4,8 @@ with open('start.bml') as file:
     text=str(file.readline()).split('=\'')[1].replace("\'","")
 import random
 behaviour_service={'tag_1':["bml_1.bml","bml_2.bml"],'tag_2':["bml_3.bml","bml_4.bml"]}
-def behaviourService(behaviour_service,tag,text,filename):
+def behaviourService(behaviour_service,tag,text):
     file=open(random.choice(behaviour_service[tag]),'r').read()
     x=file.replace("<speech>","<speech>"+text)
     return(x)
-behaviourService(behaviour_service,tag,text,'bml.bml')
+behaviourService(behaviour_service,tag,text)
