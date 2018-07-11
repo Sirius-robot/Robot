a7f8735e1723de28249c60072b09e8c758e93508
Ksenia11 <ksenia4works@gmail.com>
2018-07-11 14:32:22 +0300

    output to the second screen fnd it if fullscreen now

--- face/programm/main_1.py
+++ face/programm/main_1.py
@@ -1,28 +1,21 @@
-from collections import defaultdict
-import pygame, sys
+import pygame, win32api, win32gui, win32con, game
 from pygame.locals import *
-import game
 from eyebrows import *
 from feature import *
 from face.images import *
-import win32api, win32gui
-monitors = win32api.EnumDisplayMonitors()
-print(win32api.GetMonitorInfo(monitors[0][0]))
-print(win32api.GetMonitorInfo(monitors[1][0])['Monitor'])
-
-
+monitors = win32api.EnumDisplayMonitors() #list of monitors' coords information/
 
 pygame.init()
-surface = pygame.display.set_mode((720,480),0,32)
-(x, y, w, h) = (10,40,300,200)
-bgColor = (0,255,0)
+surface = pygame.display.set_mode((800,480),NOFRAME,32) #window's create
 
-pygame.init()
-surface = pygame.display.set_mode((720,480),0,32)
-win32gui.MoveWindow(surface, win32api.GetMonitorInfo(monitors[1][0])['Monitor'][0],
+hwnd = win32gui.GetForegroundWindow()    #get id of the top window(hwnd is integer)
+
+win32gui.ShowWindow(hwnd, win32con.SHOW_FULLSCREEN)   #fullscreen
+
+win32gui.MoveWindow(hwnd, win32api.GetMonitorInfo(monitors[1][0])['Monitor'][0],    #move this window
                     win32api.GetMonitorInfo(monitors[1][0])['Monitor'][1],
-                    win32api.GetMonitorInfo(monitors[1][0])['Monitor'][2],
-                    win32api.GetMonitorInfo(monitors[1][0])['Monitor'][3],0)
+                    800,
+                    480,0)
 (x, y, w, h) = (10,40,300,200)
 (x1, y1, w1, h1) = (50,100,300,200)
 bgColor = (0,255,0)
@@ -56,9 +49,6 @@ while mainLoop:
             if event.key == pygame.K_1:
                 sm.change(300, 300,100,100,'..\images\eyebrow.png' )
 
-        #elif event.type == pygame.KEYUP:
-        #    for handler in keyup_handlers[event.key]:
-        #        handler(event.key)
     surface.fill(bgColor)
     for i in objects:
         i.draw(surface)
