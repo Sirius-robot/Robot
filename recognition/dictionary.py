971bcb7e49c5569244f351b74732bbcd102b15b5
Vukong <40642450+Vukong@users.noreply.github.com>
2018-07-11 17:17:18 +0300

    Def version of dictionary
    Fixed bags.

--- recognition/dictionary.py
+++ recognition/dictionary.py
@@ -1,9 +1,10 @@
 import random
+
 behaviour_service={'tag_1':["bml_1.bml","bml_2.bml"],'tag_2':["bml_3.bml","bml_4.bml"]}
+
 def behaviourService(behaviour_service,tag,text):
-#tag-key for search necessary array in dictionary; text- speech for robot(result of work chatbot)"
+    #tag-key for search necessary array in dictionary; text- speech for robot(result of work chatbot)"
     file=open(random.choice(behaviour_service[tag]),'r').read()
-    x=file.replace("<speech>","<speech>"+text)
-#y-str with finish commands for robot(speech,gesture)"
+    y=file.replace("<speech>","<speech>"+text)
+    #y-str with finish commands for robot(speech,gesture)"
     return(y)
-str_f=behaviourService(behaviour_service,tag,text)
