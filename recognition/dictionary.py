5b574de8d447ddb3e28788a3d96bc24a1c32710d
Vukong <40642450+Vukong@users.noreply.github.com>
2018-07-11 10:44:30 +0300

    Def version of dictionary

--- recognition/dictionary.py
+++ recognition/dictionary.py
@@ -5,7 +5,7 @@
 import random
 behaviour_service={'tag_1':["bml_1.bml","bml_2.bml"],'tag_2':["bml_3.bml","bml_4.bml"]}
 def behaviourService(behaviour_service,tag,text):
-"tag-key for search necessary array in dictionary; text- speech for robot(result of work chatbot"
+"tag-key for search necessary array in dictionary; text- speech for robot(result of work chatbot)"
     file=open(random.choice(behaviour_service[tag]),'r').read()
     x=file.replace("<speech>","<speech>"+text)
     return(x)
