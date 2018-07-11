c3d632cde884198273b4c3a47a5354ce4298184e
Vukong <40642450+Vukong@users.noreply.github.com>
2018-07-11 17:19:06 +0300

    Function for parsing bml files
    Fixed bags.

--- recognition/parsing_bml.py
+++ recognition/parsing_bml.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+
 def dictionary_result(str_f):
     #str_f - variable after work def behaviourService
     soup=BeautifulSoup(str_f,'lxml')
@@ -6,5 +7,5 @@ def dictionary_result(str_f):
     text_1=[str(x).replace("<figure name=\"","").replace("</figure>","").replace("\">","") for x in soup.select("figure")]
     text_2=[str(x).replace("<mouth ing=\"","").replace("</mouth>","").replace("\">","") for x in soup.select("mouth")]
     text_3=[str(x).replace("<brows ing=\"","").replace("</brows>","").replace("\">","") for x in soup.select("brows")]
-    dict={'speech':[text_0],'figure':[text_1],'mouth':[text_2],'brows':[text_3]}
+    dict={'speech':text_0,'figure':text_1,'mouth':text_2,'brows':text_3}
     return(dict)
