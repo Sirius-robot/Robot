2c197bed123cfe1eddeb984d1e74adbd6c64ea53
Vukong <40642450+Vukong@users.noreply.github.com>
2018-07-11 10:42:42 +0300

    Def version of dictionary

--- recognition/dictionary_1.py
+++ recognition/dictionary_1.py
@@ -1,11 +1,12 @@
-with open('start.bml') as file:
+"with open('start.bml') as file:"
     "start.bml-file after chatbot"
-    tag=str(file.readline()).split('=\'')[1].replace("'\n","")
-    text=str(file.readline()).split('=\'')[1].replace("\'","")
+    "tag=str(file.readline()).split('=\'')[1].replace("'\n","")"
+    "text=str(file.readline()).split('=\'')[1].replace("\'","")"
 import random
 behaviour_service={'tag_1':["bml_1.bml","bml_2.bml"],'tag_2':["bml_3.bml","bml_4.bml"]}
 def behaviourService(behaviour_service,tag,text):
+"tag-key for search necessary array in dictionary; text- speech for robot(result of work chatbot"
     file=open(random.choice(behaviour_service[tag]),'r').read()
     x=file.replace("<speech>","<speech>"+text)
     return(x)
-behaviourService(behaviour_service,tag,text)
+"behaviourService(behaviour_service,tag,text)"
