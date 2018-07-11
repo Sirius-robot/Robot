6fabeb8d11ee082f9c080b517ff071e766c0024c
Dreyk <andr.isachenko@gmail.com>
2018-07-11 15:37:22 +0300

    - cancel changes of Ksenia's revert commit! Dinamixel_module files restored

--- Dynamixel_sevo_module/botcontrol.py
+++ Dynamixel_sevo_module/botcontrol.py
@@ -1,45 +1,46 @@
 import dynamixel
-from time import sleep
+
 z=[1,2,3,4,5,6]
+
 for x in z:
  dynamixel.init(x)
-pos=[0,0,0,0,0,0]
-q=0
-w=1
-for x in z:
- rpos = dynamixel.read(w)
- dgd = int(rpos/1023*300)
- pos[q] = dgd
- print(pos[q])
- q=q+1
- w=w+1
-def robotControl(ID,Time,Dego):
-    q=0
-    w=1
+
+def robotControl(ID, Time, Dego):#ID: write id of servo; Time: write move time(ms);Dego: write servo move degress
+    y = 0
+    for k in ID:
+        y = y + 1
+    y = int(y / 2)
+    q=0#Massive position index
+    w=1#ID index
+    pos=[0,0,0,0,0,0]
+    for x in ID:
+        rpos = dynamixel.read(w)
+        dgd = int(rpos/1023*300)
+        pos[q] = dgd
+        print(pos[q])
+        q=q+1
+        w=w+1
+    q=0#Massive position index
+    w=1#ID index
     Deg=[0,0,0,0,0,0]
-    for f in pos:
+    for f in ID:
         Deg[q]=abs(Dego[q]-pos[q])
         print(Deg[q])
         q = q + 1
-    q=0
-    w=0
-    e=1
-    r=0
-    t=1
-    for h in range(3):
+    q=0 #Massive position index
+    dgs=[] #dynamixel goal speed
+    dgd=[] #dynamixel goal deg
+
+    for h in ID:
         dgp=Deg[q]
         dgt=Time[q]
         dgts = Dego[q]
-        dgp2 = Deg[t]
-        dgt2 = Time[t]
-        dgts2 = Dego[t]
-        dgs = int((dgp*1023/300)/(dgt*2))
-        dgd = int(dgts*1023/300)
-        dgs2 = int((dgp2*1023/300)/(dgt2*2))
-        dgd2 = int(dgts2*1023/300)
-        dynamixel.multiMove((ID[w],ID[e]),(dgd,dgd2),(dgs,dgs2))
-        q=q+2
-        w=w+2
-        e=e+2
-        r=r+2
-        t=t+2
+        dgs.append(int((dgp*1023/300)/(dgt*2)))
+        dgd.append(int(dgts*1023/300))
+        if dgd[q] > 1023: dgd[q] = 1023
+        if dgs[q] > 1023: dgs[q] = 1023
+        if dgd[q] <= 0: dgd[q] = 1
+        if dgs[q] <= 0: dgs[q] = 1
+        q=q+1
+        w=w+1
+    dynamixel.multiMove(ID,dgd,dgs)
