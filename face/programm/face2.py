c00e27a611edc8906acdc51a77fd59cbfd92bc7c
Ksenia11 <ksenia4works@gmail.com>
2018-07-12 14:28:31 +0300

    images new:

---
+++ face/programm/face2.py
@@ -0,0 +1,100 @@
+import pygame
+import win32gui, win32api,win32con
+
+monitors = win32api.EnumDisplayMonitors()
+
+# Define some colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+# Call this function so the Pygame library can initialize itself
+pygame.init()
+
+screen = pygame.display.set_mode([800, 480],pygame.NOFRAME)
+
+hmdn = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(hmdn, win32con.SHOW_FULLSCREEN)
+print(pygame.display.Info())
+win32gui.MoveWindow(hmdn, win32api.GetMonitorInfo(monitors[1][0])['Monitor'][0],
+                    win32api.GetMonitorInfo(monitors[1][0])['Monitor'][1],800,480,0)
+
+clock = pygame.time.Clock()
+
+# Set positions of graphics
+background_position = [0, 0]
+
+# Load and set up graphics.
+background = pygame.image.load("background.png")
+background_image = pygame.image.load("eye_socket.png")
+
+pupil = pygame.image.load("pupil.png")
+eyebrows = pygame.image.load("eyebrows.png")
+eyebrows2 = pygame.image.load("eyebrows.png")
+eyebrows_anger = pygame.image.load("eyebrows_anger.png")
+eyebrows_embarrassment = pygame.image.load("eyebrows_embarrassment.png")
+eyebrows_surprise = pygame.image.load("eyebrows_surprise.png")
+mouth = pygame.image.load("mouth.png")
+mouth2 = pygame.image.load("mouth.png")
+mouth_anger = pygame.image.load("mouth_anger.png")
+mouth_embarrassment = pygame.image.load("mouth_embarrassment.png")
+mouth_boredom = pygame.image.load("mouth_boredom.png")
+mouth_surprise = pygame.image.load("mouth_surprise.png")
+'''
+pupil.set_colorkey(WHITE)
+eyebrows.set_colorkey(WHITE)
+eyebrows2.set_colorkey(WHITE)
+eyebrows_anger.set_colorkey(WHITE)
+eyebrows_surprise.set_colorkey(WHITE)
+eyebrows_embarrassment.set_colorkey(WHITE)
+mouth.set_colorkey(WHITE)
+mouth2.set_colorkey(WHITE)
+mouth_anger.set_colorkey(WHITE)
+mouth_embarrassment.set_colorkey(WHITE)
+mouth_boredom.set_colorkey(WHITE)
+mouth_surprise.set_colorkey(WHITE)
+'''
+done = False
+
+while not done:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if  event.key == pygame.K_q:
+                eyebrows = pygame.transform.rotate(eyebrows2,0)
+            if  event.key == pygame.K_w:
+                eyebrows = pygame.transform.rotate(eyebrows_anger,0)
+            if  event.key == pygame.K_e:
+                eyebrows = pygame.transform.rotate(eyebrows_embarrassment,0)
+            if  event.key == pygame.K_r:
+                eyebrows = pygame.transform.rotate(eyebrows_surprise,0)
+            if  event.key == pygame.K_a:
+                mouth = pygame.transform.rotate(mouth2,0)
+            if  event.key == pygame.K_s:
+                mouth = pygame.transform.rotate(mouth_anger,0)
+            if  event.key == pygame.K_d:
+                mouth = pygame.transform.rotate(mouth_embarrassment,0)
+            if event.key == pygame.K_f:
+                    mouth = pygame.transform.rotate(mouth_boredom, 0)
+            if event.key == pygame.K_g:
+                        mouth = pygame.transform.rotate(mouth_surprise, 0)
+
+    screen.blit(background, [0, 0])
+
+    screen.blit(pupil, [0, 0])
+
+    screen.blit(background_image, background_position)
+
+    screen.blit(eyebrows, [0, 0])
+
+
+    screen.blit(mouth, [0, 0])
+
+
+
+
+    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
--- face/programm/pr_timer.py
+++ face/programm/pr_timer.py
@@ -20,25 +20,39 @@ win32gui.MoveWindow(hwnd, win32api.GetMonitorInfo(monitors[1][0])['Monitor'][0],
 (x1, y1, w1, h1) = (50,100,300,200)
 bgColor = (0,255,0)
 
-eyebr = Feature(x, y, w, h, '..\images\eyebrow.png')
+# Load and set up graphics.
+background = pygame.image.load("../images/background.png")
+background_image = pygame.image.load("../images/eye_socket.png")
+
+pupil = pygame.image.load("../images/pupil.png")
+eyebrows = pygame.image.load("../images/eyebrows.png")
+eyebrows2 = pygame.image.load("../images/eyebrows.png")
+eyebrows_anger = pygame.image.load("../images/eyebrows_anger.png")
+eyebrows_embarrassment = pygame.image.load("../images/eyebrows_embarrassment.png")
+eyebrows_surprise = pygame.image.load("../images/eyebrows_surprise.png")
+mouth = pygame.image.load("../images/mouth.png")
+mouth2 = pygame.image.load("../images/mouth.png")
+mouth_anger = pygame.image.load("../images/mouth_anger.png")
+mouth_embarrassment = pygame.image.load("../images/mouth_embarrassment.png")
+mouth_boredom = pygame.image.load("../images/mouth_boredom.png")
+mouth_surprise = pygame.image.load("../images/mouth_surprise.png")
+
+
+eyebr = Feature(x, y, w, h, '../images/eyebrow.png')
 eyebr.draw(surface)
-sm = Feature(x1, y1, w1, h1, '..\images\eyebrow.png')
+sm = Feature(x1, y1, w1, h1, '../images/eyebrow.png')
 sm.draw(surface)
 
-objects = [eyebr, sm]
+objects = [background, pupil, background_image, eyebrows, mouth, eyebr, sm]
 
-
-MOVE_SIDE = 1000
-MOVE_DOWN = 3500
-clock = pygame.time.Clock()
-print(type(pygame.USEREVENT))
-print(pygame.USEREVENT)
-move_side_event = pygame.USEREVENT + 1
-move_down_event = pygame.USEREVENT + 2
-reloaded_event  = pygame.USEREVENT + 3
-print(pygame.KEYDOWN)
-pygame.time.set_timer(move_side_event, MOVE_SIDE)
-pygame.time.set_timer(move_down_event, MOVE_DOWN)
+#MOVE_SIDE = 1000
+#MOVE_DOWN = 3500
+#clock = pygame.time.Clock()
+#move_side_event = pygame.USEREVENT + 1
+#move_down_event = pygame.USEREVENT + 2
+#reloaded_event  = pygame.USEREVENT + 3
+#pygame.time.set_timer(move_side_event, MOVE_SIDE)
+#pygame.time.set_timer(move_down_event, MOVE_DOWN)
 
 
 
@@ -48,8 +62,6 @@ mainLoop = True
 while mainLoop:
     #clock.tick(1)
     #pygame.time.set_timer(eyebr.move(-10, 0), 10)
-
-
     for event in pygame.event.get():
         if event.type == move_side_event:
             sm.move(10, 0)
@@ -67,17 +79,36 @@ while mainLoop:
             if event.key == pygame.K_LEFT:
                 sm.move(-10, 0)
 
-            if event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT:
                 sm.move(10, 0)
 
-            if event.key == pygame.K_UP:
+            elif event.key == pygame.K_UP:
                 sm.move(0, -10)
 
-            if event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_DOWN:
                 sm.move(0, 10)
 
-            if event.key == pygame.K_1:
-                sm.change(300, 300,100,100,'..\images\eyebrow.png' )
+            elif event.key == pygame.K_1:
+                sm.change(300, 300,100,100,'../images/eyebrow.png' )
+
+            elif  event.key == pygame.K_q:
+                eyebrows = pygame.transform.rotate(eyebrows2,0)
+            elif  event.key == pygame.K_w:
+                eyebrows = pygame.transform.rotate(eyebrows_anger,0)
+            elif  event.key == pygame.K_e:
+                eyebrows = pygame.transform.rotate(eyebrows_embarrassment,0)
+            elif  event.key == pygame.K_r:
+                eyebrows = pygame.transform.rotate(eyebrows_surprise,0)
+            elif  event.key == pygame.K_a:
+                mouth = pygame.transform.rotate(mouth2,0)
+            elif  event.key == pygame.K_s:
+                mouth = pygame.transform.rotate(mouth_anger,0)
+            elif  event.key == pygame.K_d:
+                mouth = pygame.transform.rotate(mouth_embarrassment,0)
+            elif event.key == pygame.K_f:
+                mouth = pygame.transform.rotate(mouth_boredom, 0)
+            elif event.key == pygame.K_g:
+                mouth = pygame.transform.rotate(mouth_surprise, 0)
 
     surface.fill(bgColor)
     for i in objects:
