4dc912c5a1580ad94085ae1a037832b32d02c8f8
Raptor701 <contactadress456@gmail.com>
2018-07-12 05:05:01 -0400

    Delete test functions

--- recognition/sintethys.py
+++ recognition/sintethys.py
@@ -1,12 +1,12 @@
 import requests
-#import winsound
 import configparser
+
 config = configparser.ConfigParser()
 config.read("../settings.ini")
 key = config.get("Settings", "API_KEY")
+
 def speech(text):
 	URL = 'https://tts.voicetech.yandex.net/generate?text='+text+'&format=wav&lang=ru-RU&speaker=ermil&key='+key+'&speed=1&emotion=good'
-        #print(URL)
 	response=requests.get(URL)
 	if response.status_code==200:
 		return response.content
@@ -14,9 +14,6 @@ def speech(text):
 		raise Exception("Cant synthesize speech"+response.status_code)
 
 
-bina = speech('Privet Mir')
-file = open("testfile.wav","w") 
-file.write(bina) 
-file.close() 
+
 
 
