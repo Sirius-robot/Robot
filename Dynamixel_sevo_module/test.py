e35588d1bbc7499dd7c9c21843f5bb380a10756a
Danil <denner02@mail.ru>
2018-07-13 14:33:21 +0300

    Code with use DataBase(servo)

--- Dynamixel_sevo_module/test.py
+++ Dynamixel_sevo_module/test.py
@@ -2,51 +2,34 @@ import botcontrol
 from DataBase import *
 import threading
 #database.data[i][1][j][1]
-x = database.gesture(gesname)
-
-p=0
-l=0
-m=0
-u=0
-y = []
-c = []
-z = []
-v = []
+command_data = database.gesture(gesname)
+flag = 0
+#time_iter=-10
+time_dict = {}
 
 #database.del_gesture(TITLE, gesname)
-for k in x:
-    y.append(x[u])
-  #  print(y)
-    for f in y:
-        z.append(y[p])
-        #print(z[p])
-        p=p+1
-        for g in z:
-            c.append(z[l])
-            print(c[l])
-            l=l+1
-            for h in c:
-                v.append(c[m])
-                m=m+1
-
-
-
-
-y0=x[0]
-y1=x[1]
-y2=x[2]
-z1=y0[1]
-c0=z1[1] #module
-v0=z1[2] #var
-print(z1[1])
-
+for commands_time in command_data:
+    time = commands_time[0]
+    commands = commands_time[1]
+    ids = []
+    angles = []
+    dif_times = []
+    for command in commands:
+        ids.append(command[0])
+        angles.append(command[1])
+        dif_times.append(command[2])
 database.del_gesture(TITLE, gesname)
 
-#botcontrol.multiInt((1,2,3,4))
-#botcontrol.robotControl((1,2,3,4),(1023,6000,10000,20000),(30,300,300,300))
+botcontrol.multiInt((1,2,3,4))
+botcontrol.robotControl((1,2,3,5),(1023,6000,10000,20000),(300,300,300,150))
 
-def timer():
-    threading.Timer(0.04,botcontrol.robotControl((1,2,3,4),(1023,6000,10000,20000),(30,300,300,300))).start()
-    threading.Timer(0.04,timer).start()
-    print("Hi")
-#timer()
+def timer1():
+    threading.Timer(0.01,timer1).start()
+    time_iter = time_iter+10
+    print(time_iter)
+    # if (time == time_iter):
+    #    time_dict[time] = [ids,angles,dif_times]
+     #   print(time_dict)
+
+
+timer1()
