2a54d3b61b17a4a58dd99ee59d82b7d165be12fb
Danil <denner02@mail.ru>
2018-07-15 16:17:25 +0300

    Servo code update

--- Dynamixel_sevo_module/test.py
+++ Dynamixel_sevo_module/test.py
@@ -1,10 +1,11 @@
 import botcontrol
-from DataBase import *
 import threading
-#database.data[i][1][j][1]
-command_data = database.gesture(gesname)
-flag = 0
-#time_iter=-10
+import sys
+sys.path.insert(0, '../Alisnky')
+from DataBase import *
+command_data = database.gesture('smail')
+time_iter = -10
+
 time_dict = {}
 
 #database.del_gesture(TITLE, gesname)
@@ -18,18 +19,35 @@ for commands_time in command_data:
         ids.append(command[0])
         angles.append(command[1])
         dif_times.append(command[2])
-database.del_gesture(TITLE, gesname)
+        time_dict[time] = [ids, angles, dif_times]
+#database.del_gesture(TITLE, gesname)
+#print(time_dict)
+botcontrol.multiInt(ids)
 
-botcontrol.multiInt((1,2,3,4))
-botcontrol.robotControl((1,2,3,5),(1023,6000,10000,20000),(300,300,300,150))
+#botcontrol.multiInt((1,2,3,4,5,6))
+#botcontrol.robotControl((1,2,3,5,6),(200,400,1000,200,500),(200,300,300,300,300,30))
+lenni=0
+def work():
+    global time_iter
+    time_iter += 10
+    return time_iter
 
 def timer1():
+    global lenni
     threading.Timer(0.01,timer1).start()
-    time_iter = time_iter+10
-    print(time_iter)
-    # if (time == time_iter):
-    #    time_dict[time] = [ids,angles,dif_times]
-     #   print(time_dict)
+    d = work()
+    lenny = len(time_dict.keys())
+    if lenny == lenni:
+        print('THE END')
+        lenni+=1
+    if time_dict.get(d,666) != 666:
+        lenni = lenni + 1
+        data = time_dict[d]
+        print(time_dict.get(d))
+        print(data[0])
+        print(data[2])
+        print(data[1])
+        botcontrol.robotControl(data[0],data[2],data[1])
 
-
+#database.del_gesture(TITLE, gesname)
 timer1()
