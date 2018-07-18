fad8a9f6809bc9da1d8544c017eb91cb55937f6e
Danil <denner02@mail.ru>
2018-07-18 17:06:35 +0300

    Bug fix

--- Dynamixel_sevo_module/test.py
+++ Dynamixel_sevo_module/test.py
@@ -3,8 +3,8 @@ import threading
 import sys
 import time
 import queue
-from chbh import *
-from timer import *
+import Chbh
+import Timer
 sys.path.insert(0, '../Alisnky')
 from DataBase import *
 sys.path.insert(1, '../Sinthesis')
@@ -13,10 +13,10 @@ sys.path.insert(2, '../recognition')
 from parsing_bml import *
 
 def m1():
-    master()
+    Chbh.master()
 
 def s1():
-    slaver()
+    Timer.slaver()
 
 mastert = threading.Thread(target=m1)
 mastert.start()
