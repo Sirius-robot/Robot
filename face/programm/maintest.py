f3810dc2c498c17f6f97f789ef9ab6f2c29fa9e2
Ksenia11 <ksenia4works@gmail.com>
2018-07-18 14:56:50 +0300

    maintest - main thread. test_thread and mypygame - another threads

--- face/programm/maintest.py
+++ face/programm/maintest.py
@@ -1,16 +1,27 @@
 from queue import Queue
 import queue, pygame, win32api, win32gui, win32con, time
-from threading import Thread
+import test_thread, mypygame
+from threading import Thread, Event
 from pygame.locals import *
 from feature11 import Face
-from DataBase import *
-from test_thread import *
 from pygame import *
 
-waitEvent = True
+waitEvent = Event()
+waitEvent.set()         #waitEvent.isSet() return True
+
 que = Queue()
 que_pup = Queue()
 
-pygame_thread = Thread(target=pygame.main_pygame)
-pygame_thread.start()
-pygame_thread.join()
+pygame_thread_mypygame= Thread(target=mypygame.main_pygame, args = (que, que_pup, waitEvent))
+pygame_thread_mypygame.start()
+
+pygame_thread_putinqu = Thread(target=test_thread.putinqu, args = (que, que_pup, waitEvent))
+pygame_thread_putinqu.start()
+print("before join")
+try:
+    pygame_thread_putinqu.join()
+    pygame_thread_mypygame.join()
+except KeyboardInterrupt:
+    print("interrupt")
+    waitEvent.clear()
+print("after join")
