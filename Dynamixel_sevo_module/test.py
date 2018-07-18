7e847e712fb947ad9f4584ba3ce0e07c783e4cbf
Danil <denner02@mail.ru>
2018-07-18 10:42:54 +0300

    Just commit 5

--- Dynamixel_sevo_module/test.py
+++ Dynamixel_sevo_module/test.py
@@ -2,6 +2,7 @@ import botcontrol
 import threading
 import sys
 import time
+import queue
 
 sys.path.insert(0, '../Alisnky')
 from DataBase import *
@@ -9,39 +10,50 @@ sys.path.insert(1, '../Sinthesis')
 from sinthesis import *
 sys.path.insert(2, '../recognition')
 from parsing_bml import *
+
 time_iter = -10
+lenni = 0
+q = queue.Queue()
 
 def master():
+
     db = database()
     while 1:
         f = open('vvv.bml', 'r')
         z = f.read()
         x = dictionary_result(z)
-        print(x[0])
         gestur = x['figure']
-        speech = x['speech']
-        text = speech
+        texts = x['speech']
+        if len(gestur) > 0:
+            if len(gestur[0]) > 0:
+                command_data = db.gesture(gestur[0])
+                time_dict = {}
+                for commands_time in command_data:
+                    timew = commands_time[0]
+                    commands = commands_time[1]
+                    ids = []
+                    angles = []
+                    dif_times = []
+                    for command in commands:
+                        ids.append(command[0])
+                        angles.append(command[1])
+                        dif_times.append(command[2])
+                        time_dict[timew] = [ids, angles, dif_times]  # очередь
+                q.put(time_dict)
+        if len(texts) > 0:
+            if len(texts[0])>0:
+                print(texts[0])
+                audio = speech(texts[0])
+                winsound.PlaySound(audio, winsound.SND_MEMORY)
         # sinthesis.speech(text)
-        audio = speech(text)
-        winsound.PlaySound(audio, winsound.SND_MEMORY)
-        command_data = db.gesture(gestur)
-        time_dict = {}
-        for commands_time in command_data:
-            time = commands_time[0]
-            commands = commands_time[1]
-            ids = []
-            angles = []
-            dif_times = []
-            for command in commands:
-                ids.append(command[0])
-                angles.append(command[1])
-                dif_times.append(command[2])
-                time_dict[time] = [ids, angles, dif_times]#очередь
-
-        sleep(5)
+        #if text =
+        time.sleep(5)
 
 def slaver():
-
+    time_dict = q.get()
+    botcontrol.multiInt((1, 2, 3, 4, 5, 6))
+   # botcontrol.robotControl((1,2,4,5,6),(400,1000,200,500,500),(300,300,300,300,30))
+    print(time_dict)
     def work():
         global time_iter
         time_iter += 10
@@ -67,5 +79,5 @@ def slaver():
 
 mastert = threading.Thread(target=master)
 mastert.start()
-#slavert = threading.Thread(target=slaver)
-#slavert.start()
+slavert = threading.Thread(target=slaver)
+slavert.start()
